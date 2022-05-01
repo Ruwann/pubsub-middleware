a491c7619a91f4fa788e85e976350209be6d9616
Ruwan <ruwanlambrichts@gmail.com>
2022-05-01 19:12:39 +0200

    Only do decoding for POST requests

--- middleware.py
+++ middleware.py
@@ -13,33 +13,34 @@ class PubsubMiddleware:
     def __call__(self, environ, start_response):
         iterable = None
 
-        # print(environ)
-        # when running under gunicorn
-        # print(environ['wsgi.input'])  # gunicorn.http.body.Body
-        # print(environ['wsgi.input'].reader)  # gunicorn.http.body.LengthReader
-        # print(environ['wsgi.input'].buf)  # _io.BytesIO
+        if environ['REQUEST_METHOD'] == 'POST':
+            # print(environ)
+            # when running under gunicorn
+            # print(environ['wsgi.input'])  # gunicorn.http.body.Body
+            # print(environ['wsgi.input'].reader)  # gunicorn.http.body.LengthReader
+            # print(environ['wsgi.input'].buf)  # _io.BytesIO
 
-        length = int(environ.get('CONTENT_LENGTH', '0'))
-        request_stream = environ['wsgi.input']
-        body = request_stream.read(length)
-        # body = request_stream.peek()
-        message = json.loads(body)
-        data = message['message']['data']
-        pubsub_message = base64.b64decode(data)
+            length = int(environ.get('CONTENT_LENGTH', '0'))
+            request_stream = environ['wsgi.input']
+            body = request_stream.read(length)
+            # body = request_stream.peek()
+            message = json.loads(body)
+            data = message['message']['data']
+            pubsub_message = base64.b64decode(data)
 
-        wsgi_input = io.BytesIO(pubsub_message)
-        content_length = wsgi_input.getbuffer().nbytes
-        wsgi_input.seek(0)
-        # TODO: which class to use
-        #   werkzeug dev server uses io.BufferedReader
-        #   gunicorn uses gunicorn.http.body.Body
-        #       https://github.com/benoitc/gunicorn/blob/master/gunicorn/http/body.py#L177
-        wsgi_input = io.BufferedReader(wsgi_input)
-        environ['wsgi.input'] = wsgi_input
-        # change content_length
-        environ['CONTENT_LENGTH'] = content_length
-        # change content_type
-        environ['CONTENT_TYPE'] = self.content_type
+            wsgi_input = io.BytesIO(pubsub_message)
+            content_length = wsgi_input.getbuffer().nbytes
+            wsgi_input.seek(0)
+            # TODO: which class to use
+            #   werkzeug dev server uses io.BufferedReader
+            #   gunicorn uses gunicorn.http.body.Body
+            #       https://github.com/benoitc/gunicorn/blob/master/gunicorn/http/body.py#L177
+            wsgi_input = io.BufferedReader(wsgi_input)
+            environ['wsgi.input'] = wsgi_input
+            # change content_length
+            environ['CONTENT_LENGTH'] = content_length
+            # change content_type
+            environ['CONTENT_TYPE'] = self.content_type
 
         try:
             iterable = self.application(environ, start_response)
